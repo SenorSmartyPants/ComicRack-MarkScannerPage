a3fbb3bda1de81de78fa2ffc7d551c11df062c93
SenorSmartyPants <senorsmartypants@gmail.com>
2022-02-08 21:28:01 -0600

    Compatibility with Combine Script creator

--- MarkScannerPage.py
+++ MarkScannerPage.py
@@ -4,8 +4,6 @@
 #
 # You are free to modify and distribute this file
 ##########################################################################
-from utils import *
-
 from System.IO import Path
 
 import clr
@@ -13,6 +11,18 @@ clr.AddReferenceByPartialName('ComicRack.Engine')
 from cYo.Projects.ComicRack.Engine import ComicPageType
 
 markScannerAs = ComicPageType.Deleted
+
+# routine copied from https://gist.github.com/mxu007/4209efa6d6e79e3bce17ab6ce5679fb9#file-lcp_1-py
+def longestCommonPrefix(strs):
+    longest_pre = ""
+    if not strs: return longest_pre
+    shortest_str = min(strs, key=len)
+    for i in range(len(shortest_str)):
+        if all([x.startswith(shortest_str[:i+1]) for x in strs]):
+            longest_pre = shortest_str[:i+1]
+        else:
+            break
+    return longest_pre
 
 def getPageNameList(book):
 	pageNameList = []
